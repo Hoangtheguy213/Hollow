92fc53cfe2f2027d60c69f1a46a5be8902594435
Hoangtheguy213 <nkh29793@gmail.com>
2024-05-30 01:55:48 +0700

    feat: update parallax + map

--- Assets/Scripts/BackgroundController.cs
+++ Assets/Scripts/BackgroundController.cs
@@ -5,19 +5,32 @@ using UnityEngine;
 public class BackgroundController : MonoBehaviour
 {
     // Start is called before the first frame update
-    private float startPos;
+    private float startPos, length;
     public GameObject cam;
     public float ParallaxEffect;
 
     void Start()
     {
         startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        // tinh toan khoang cach cua background dua tren toc do di chuyen cua camera
         float distance = cam.transform.position.x * ParallaxEffect; //0= move with camera, 1= not move, 0,5 = half speed
+        float movement = cam.transform.position.x * (1 - ParallaxEffect);  
+
         transform.position = new Vector3(startPos +distance, transform.position.y, transform.position.z);
+        if (movement >startPos + length)
+        {
+            startPos += length;
+        }
+        else if(movement <startPos - length)
+        {
+            startPos -= length;
+        }
     }
+    
 }
